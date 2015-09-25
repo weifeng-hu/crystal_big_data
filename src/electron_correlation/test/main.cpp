6d441e261ac181bebffb22cd378162c271f7b59d
Weifeng Hu <hulakui@gmail.com>
2015-09-24 20:37:04 -0400

    + Checkpoint    + add the electron correlation test code
     On branch manybody_expansion
     Your branch is up-to-date with 'origin/manybody_expansion'.
    
     Changes to be committed:
    	new file:   main.cpp
    	new file:   makefile
    
     Untracked files:
    	../../../bin/
    	../../../examples/
    	../../analytic_gradient/
    	../../cphf/cphf_aries/
    	main.o
    	test
    	../../fci/fci_lyra/
    	../../fci/fci_vega/
    	../../integral/trsf_norma/
    	../../nonadiabatic_coupling/
    	../../numerical_nonadiabatic_coupling/
    	../../python_test/
    	../../structure/test/main.o
    	../../structure/test/test
    	../../utilities/solid_gen/
    	../../../test/fragment_generator/
    	../../../test/nonadiabatic_coupling/
    	../../../test/numerical_nonadiabatic_coupling/
    	../../../utilities/

---
+++ src/electron_correlation/test/main.cpp
@@ -0,0 +1,41 @@
+/**
+ * @file
+ * @author Weifeng Hu
+ *
+ * @section LICENSE
+ *
+ * Copyright (C) 2013, 2014, 2015  Weifeng Hu
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @section DESCRIPTION
+ *
+ *
+ */
+
+#include <electron_correlation/report.hpp>
+#include <electron_correlation/client.hpp>
+#include <electron_correlation/setting.hpp>
+
+int main( int argc, char* argv[] ) {
+
+  using iquads :: electron_correlation :: Report;
+  using iquads :: electron_correlation :: Client;
+  using iquads :: electron_correlation :: Setting;
+
+  Report report;
+  Setting setting;
+  Client client;
+
+} // end of main()
