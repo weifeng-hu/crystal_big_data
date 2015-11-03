ed028289f4fed1b4e7a7b89131586689f3420f97
Weifeng Hu <hulakui@gmail.com>
2015-11-02 19:25:36 -0500

    On branch manybody_expansion  Your branch is up-to-date with 'origin/manybody_expansion'.
     + Checkpoint
      + add a simple progress bar
    
     Changes to be committed:
    	new file:   progress_display.hpp
    
     Untracked files:
    	../../bin/
    	../chemistry/test/main.o
    	../chemistry/test/test_element
    	../electron_correlation/client.o
    	../electron_correlation/libiquads_electron_correlation.a
    	../electron_correlation/report.o
    	../electron_correlation/setting.o
    	../electron_correlation/test/client.o
    	../electron_correlation/test/input/
    	../electron_correlation/test/main.o
    	../electron_correlation/test/output/
    	../electron_correlation/test/report.o
    	../electron_correlation/test/scratch/
    	../electron_correlation/test/setting.o
    	../electron_correlation/test/test_electron_correlation
    	../file_system/test/main.o
    	../file_system/test/test_file_system
    	../interface_to_third_party/libiquads_third_party.a
    	../interface_to_third_party/molpro_agent.o
    	../interface_to_third_party/test/molpro_agent.o
    	../interface_to_third_party/test/test_molpro_agent
    	../manybody_expansion/test/.exrc
    	../manybody_expansion/test/main.o
    	../manybody_expansion/test/test
    	../manybody_expansion/test/test.11x11x11
    	../manybody_expansion/test/test.13x13x13
    	../manybody_expansion/test/test.3.order.3x3x3
    	../manybody_expansion/test/test.3x1x1
    	../manybody_expansion/test/test.3x3x1
    	../manybody_expansion/test/test.3x3x3
    	../manybody_expansion/test/test.5x5x5
    	../manybody_expansion/test/test.7x7x7
    	../manybody_expansion/test/test.9x9x9
    	../mbe_checkout.sh
    	../structure/test/main.o
    	../structure/test/test
    	../utility/test/main.o
    	../utility/test/test_utility
    	../../test/nonadiabatic_coupling/
    	../../test/numerical_nonadiabatic_coupling/

---
+++ src/timer/progress_display.hpp
@@ -0,0 +1,85 @@
+/**
+ * @file
+ * @author Weifeng Hu
+ *
+ * @section LICENSE
+ *
+ * Copyright (C) 2013, 2014, 2015  Weifeng Hu
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @section DESCRIPTION
+ *
+ *
+ */
+
+#ifndef PROGRESS_DISPLAY_HPP
+#define PROGRESS_DISPLAY_HPP
+
+#include <vector>
+#include <array>
+#include <iostream>
+
+namespace iquads {
+
+namespace timer {
+
+class ProgressDisplay {
+public:
+  typedef ProgressDisplay this_type;
+
+public:
+  ProgressDisplay( size_t total_step ) {
+    this->current_step_ = 0;
+    this->total_step_   = total_step;
+    this->n_tic_ = 50;
+    std :: cout << "|----|----|----|----|----|----|----|----|----|----|" << std :: endl;
+  };
+
+public:
+  this_type& operator++() {
+    this->current_step_ += 1;
+    this->update_progress_bar();
+    return *this;
+  }
+  this_type operator++( int ) {
+    this_type tmp( *this );
+    this->operator++();
+    return tmp;
+  }
+
+private:
+  void update_progress_bar() {
+    int number_of_tics_to_show = (int) ( ( double )( current_step_/total_step_ ) * 50.0e0 );
+    std :: cout << "\r";
+    for( int i = 0; i < number_of_tics_to_show; i++ ) {
+      std :: cout << "*";
+    }
+    if( current_step_ == this->total_step_ ) {
+      std :: cout << "*" << std :: endl;
+    }
+  }
+
+private:
+  size_t current_step_;
+  size_t total_step_;
+  const size_t n_tic_;
+
+}; // end of class ProgressDisplay
+
+} // end of namespace timer
+
+} // end of namespace iquads
+
+#endif
